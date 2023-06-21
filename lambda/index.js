9df26a3e063d6e044f24f57f4ee658d0b5329b60
USEN GIT CONFIG --LOCAL <USEN GIT CONFIG --LOCAL>
2023-06-20 20:43:00 -0600

    commit inicial

---
+++ lambda/index.js
@@ -0,0 +1,15 @@
+const Alexa = require('ask-sdk-core');
+
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
